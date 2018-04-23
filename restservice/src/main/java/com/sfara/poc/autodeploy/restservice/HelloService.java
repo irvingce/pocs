13c3d66258aa5d0f797c57fff12c398ee1e929ca
irvingcesf <irving.casillas@sfara.com>
2018-04-23 17:03:28 -0500

    Create test RESTFul project.

---
+++ restservice/src/main/java/com/sfara/poc/autodeploy/restservice/HelloService.java
@@ -0,0 +1,15 @@
+package com.sfara.poc.autodeploy.restservice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("/hello")
+public class HelloService {
+
+	@GET
+	@Path("/{name}")
+	public String sayHello(@PathParam("name") String name) {
+		return "Hello "+name;
+	}
+}
